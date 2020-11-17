eecbb98785521c46ca29b6a13638b75902301295
esodermark <sodermark@live.se>
2020-11-17 12:43:17 +0100

    created api calls for user + posts

--- src/api-client.js
+++ src/api-client.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axiosInstance = Axios.create({
-    baseURL: "https://lab.willandskill.eu/"
+    baseURL: "https://lab.willandskill.eu/api/v1/"
 })
 
 
@@ -22,3 +22,45 @@ axiosInstance.interceptors.response.use((response) => {
 })
 
 
+// USER
+axiosInstance.Register = async(newUser) => {
+    await axiosInstance.post('/auth/users/', newUser)
+    return true
+}
+
+axiosInstance.Login = async(user) => {
+    await axiosInstance.post('/auth/api-token-auth/', user)
+    return true
+}
+
+axiosInstance.GetUserInfo = async() => {
+    const userInfo = await axiosInstance.get('/me/')
+    return userInfo
+}
+
+
+// POSTS
+axiosInstance.CreatePost = async(newPost) => {
+    await axiosInstance.post('/forum/posts/', newPost)
+    return true
+}
+
+axiosInstance.CreateReplyToPost = async(reply) => {
+    await axiosInstance.post('/forum/posts/', reply)
+    return true
+}
+
+axiosInstance.GetPostsList = async() => {
+    const postsList = await axiosInstance.get('/forum/posts/')
+    return postsList
+}
+
+axiosInstance.GetPostDetailsById = async(id) => {
+    const postDetails = await axiosInstance.get(`/forum/posts/${id}/`)
+    return postDetails
+}
+
+axiosInstance.GetPostReplyListById = async(id) => {
+    const replyList = await axiosInstance.get(`/forum/posts/${id}/`)
+    return replyList
+}
