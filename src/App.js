1bbe0884f2b630e31527fb131f3eb1beeffb54b6
esodermark <sodermark@live.se>
2020-11-17 13:30:03 +0100

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css'
+import PostContextProvider from './contexts/PostContext'
+import UserContextProvider from './contexts/UserContext'
+import { Switch, Route } from 'react-router-dom'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import PostCreate from './pages/PostCreate'
+import PostList from './pages/PostList'
+import PostDetail from './pages/PostDetail'
+import Home from './pages/Home'
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <>
+      <UserContextProvider>
+      <PostContextProvider>
+        <Switch>
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+          <Route path='/posts/create' component={PostCreate} />
+          <Route path='/posts' component={PostList} />
+          <Route path='/post/:id' component={PostDetail} />
+          <Route path='/' component={Home} />
+        </Switch>
+      </PostContextProvider>
+      </UserContextProvider>
+    </>
+  )
 }
 
 export default App
