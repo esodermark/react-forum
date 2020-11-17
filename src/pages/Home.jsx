93f63674a66eea534fd51e7c496e4094e0f9e07e
esodermark <sodermark@live.se>
2020-11-17 14:47:39 +0100

    created Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { H1, P } from '../components/baseElements/Typography'
+import { PostListLink } from './HomeElements'
+
+const Home = () => {
+    return (
+        <>
+            <H1 color="primary">Welcome to the forum</H1>
+            <P color="primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore aliquid sapiente voluptatibus ipsum, deleniti nostrum reprehenderit unde distinctio in sunt natus ab saepe culpa exercitationem. Eaque sunt perspiciatis labore temporibus?</P>
+            <PostListLink>Start reading posts now</PostListLink>
+        </>
+    )
+}
+
+export default Home
